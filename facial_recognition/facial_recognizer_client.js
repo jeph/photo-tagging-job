5d7069da2245f6baf381b1c4f24f6a9b783fb13b
Jeph <jephliu@jeph.io>
2019-12-06 14:25:54 -0500

    Use const

--- facial_recognition/facial_recognizer_client.js
+++ facial_recognition/facial_recognizer_client.js
@@ -1,24 +1,25 @@
-var PROTO_PATH = __dirname + '/detection.proto'
-  
-var grpc = require('grpc')
-var protoLoader = require('@grpc/proto-loader')
-var packageDefinition = protoLoader.loadSync(
-   PROTO_PATH,
-   {keepCase: true,
+const PROTO_PATH = __dirname + '/detection.proto'
+
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
     longs: String,
     enums: String,
     defaults: true,
     oneofs: true
-   });
+  })
 
-var detection_proto = grpc.loadPackageDefinition(packageDefinition).facial_recognition;
+const detection_proto = grpc.loadPackageDefinition(packageDefinition).facial_recognition
 
-function main() {
-   var client = new detection_proto.FacialRecognizer('localhost:50051', grpc.credentials.createInsecure());
-   client.detect({img: process.argv[2]}, function(err, response) {
-      console.log(response.faces);
-      return(response.faces);
-   });
+function main () {
+  const client = new detection_proto.FacialRecognizer('localhost:50051', grpc.credentials.createInsecure())
+  client.detect({ img: process.argv[2] }, function (err, response) {
+    console.log(response.faces)
+    return (response.faces)
+  })
 }
 
-main();
+main()
--- facial_recognition/facial_recognizer_server.js
+++ facial_recognition/facial_recognizer_server.js
@@ -1,43 +1,42 @@
-var PROTO_PATH = __dirname + '/detection.proto';
-var MODEL_URL = __dirname + '/weights';
+const PROTO_PATH = __dirname + '/detection.proto'
+const MODEL_URL = __dirname + '/weights'
 
-var faceapi = require('face-api.js');
-var canvas = require('canvas');
-var image = require('canvas');
-var imageData = require('canvas');
+const faceapi = require('face-api.js')
+const canvas = require('canvas')
 
-var { Canvas, Image, ImageData } = canvas
+const { Canvas, Image, ImageData } = canvas
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
 
-var grpc = require('grpc');
-var protoLoader = require('@grpc/proto-loader')
-var packageDefinition = protoLoader.loadSync(
-   PROTO_PATH,
-   {keepCase: true,
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
     longs: String,
     enums: String,
     defaults: true,
     oneofs: true
-   });
+  })
 
-var detection_proto = grpc.loadPackageDefinition(packageDefinition).facial_recognition;
+const detectionProto = grpc.loadPackageDefinition(packageDefinition).facial_recognition
 
-async function detect(call, callback) {
-   await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
-   var img = await canvas.loadImage(call.request.img);
-   var detections = await faceapi.detectAllFaces(img);
-   if (detections == null) {
-      callback(null, {faces: 0});
-   } else {
-      callback(null, {faces: detections.length});
-   }
+async function detect (call, callback) {
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL)
+  const img = await canvas.loadImage(call.request.img)
+  const detections = await faceapi.detectAllFaces(img)
+  if (detections == null) {
+    callback(null, { faces: 0 })
+  } else {
+    callback(null, { faces: detections.length })
+  }
 }
 
-function main() {
-   var server = new grpc.Server();
-   server.addService(detection_proto.FacialRecognizer.service, {detect: detect});
-   server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-   server.start();
+function main () {
+  const server = new grpc.Server()
+  server.addService(detectionProto.FacialRecognizer.service, { detect: detect })
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
+  server.start()
 }
 
-main();
+main()
