d48189236df19270c97f289fbf8b3b725f7266de
Jeph <jephliu@jeph.io>
2019-12-06 22:13:31 -0500

    Access photos from s3 and run model on them

--- facial_recognition/facial_recognizer_server.js
+++ facial_recognition/facial_recognizer_server.js
@@ -1,42 +1,26 @@
-const PROTO_PATH = __dirname + '/detection.proto'
 const MODEL_URL = __dirname + '/weights'
-
+const aws = require('aws-sdk')
+aws.config.loadFromPath('./config.json')
+const s3 = new aws.S3({ apiVersion: '2006-03-01', region: 'us-east-1' })
 const faceapi = require('face-api.js')
 const canvas = require('canvas')
 
 const { Canvas, Image, ImageData } = canvas
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+const Bucket = 'scalica-photos'
 
-const grpc = require('grpc')
-const protoLoader = require('@grpc/proto-loader')
-const packageDefinition = protoLoader.loadSync(
-  PROTO_PATH,
-  {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  })
-
-const detectionProto = grpc.loadPackageDefinition(packageDefinition).facial_recognition
-
-async function detect (call, callback) {
-  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL)
-  const img = await canvas.loadImage(call.request.img)
-  const detections = await faceapi.detectAllFaces(img)
-  if (detections == null) {
-    callback(null, { faces: 0 })
+s3.listObjects({ Bucket }, async function (err, data) {
+  if (err) {
+    console.error(err, err.stack)
   } else {
-    callback(null, { faces: detections.length })
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL)
+    for (let i = 0; i < data.Contents.length; i++) {
+      const img = await canvas.loadImage(`https://scalica-photos.s3.amazonaws.com/${data.Contents[i].Key}`)
+      const detections = await faceapi.detectAllFaces(img)
+      if (detections && detections.length > 0) {
+        // TODO database update
+        console.log(`Detected ${detections.length} faces`)
+      }
+    }
   }
-}
-
-function main () {
-  const server = new grpc.Server()
-  server.addService(detectionProto.FacialRecognizer.service, { detect: detect })
-  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
-  server.start()
-}
-
-main()
+})
