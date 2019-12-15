84ba2db26971146fe44d72a075c46efe8ffdada9
Jeph <jephliu@jeph.io>
2019-12-14 21:57:02 -0500

    Integreate with mysql and cron

--- facial_recognition/facial_recognizer_server.js
+++ facial_recognition/facial_recognizer_server.js
@@ -1,59 +1,60 @@
 const MODEL_URL = __dirname + '/weights'
 const aws = require('aws-sdk')
 aws.config.loadFromPath('./config.json')
-const config = require('../config');
+const dbConfig = require('../db-config')
 const s3 = new aws.S3({ apiVersion: '2006-03-01', region: 'us-east-1' })
 const faceapi = require('face-api.js')
 const canvas = require('canvas')
-//const cron = require('node-cron')
-const mysql = require('mysql');
-//require('@tensorflow/tfjs-node');
+const mysql = require('mysql')
 
 const { Canvas, Image, ImageData } = canvas
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
 const Bucket = 'scalica-photos'
 
-// cron.schedule("* */30 * * * *", function() {
-//   console.log("running a task every 30 minutes");
-//   scanPhotos();
-// });
-
-const con = mysql.createConnection(config.databaseOptions);
-
-s3.listObjects({ Bucket }, async function (err, data) {
-  if (err) {
-    console.error(err, err.stack)
-  } else {
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL)
-    con.connect((err) => {
-      if(err){
-        console.log('Error connecting to Db');
-        console.log(err);
-        return;
+const CronJob = require('cron').CronJob
+// eslint-disable-next-line no-new
+new CronJob('*/30 * * * *', function () {
+  s3.listObjects({ Bucket }, async function (err, data) {
+    const con = mysql.createConnection(dbConfig)
+    const timeCheckSql = 'SELECT * FROM last_scan_time WHERE id = 1'
+    con.query(timeCheckSql, function (error, results) {
+      if (error) {
+        console.log(error)
+      } else {
+        const timeLastUpdated = new Date(results[0].last_scan_time).getTime() - 18000000
+        const filteredData = data.Contents.filter((file) => {
+          const fileTimeStamp = file.Key.match(/([^-]+)/g)[0]
+          return timeLastUpdated < parseInt(fileTimeStamp)
+        })
+        console.log(filteredData)
+        const timeUpdateSql = 'UPDATE last_scan_time SET last_scan_time = now(3) WHERE id = 1'
+        con.query(timeUpdateSql, async function (err) {
+          if (err) {
+            console.log(err)
+          } else {
+            if (err) {
+              console.error(err, err.stack)
+            } else {
+              await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL)
+              for (let i = 0; i < filteredData.length; i++) {
+                const img = await canvas.loadImage(`https://scalica-photos.s3.amazonaws.com/${filteredData[i].Key}`)
+                const detections = await faceapi.detectAllFaces(img)
+                if (detections && detections.length > 0) {
+                  const sql = 'UPDATE micro_photo SET num_faces = ? WHERE img_id = ?'
+                  const parameters = [detections.length, filteredData[i].Key]
+                  con.query(sql, parameters, function (error) {
+                    if (error) console.log(error)
+                  })
+                  console.log(`Detected ${detections.length} faces`)
+                }
+              }
+              con.end((err) => {
+                if (err) console.log(err)
+              })
+            }
+          }
+        })
       }
-      console.log('Connection established');
-      // const sql = `INSERT INTO scalica (field1, field2) 
-      //               VALUES ('value', 'value')`;
-      // con.query(sql, function (err, result) {
-      //   if (err) throw err;
-      //   console.log("1 record inserted");
-      // });
-    });
-    
-    for (let i = 0; i < data.Contents.length; i++) {
-      const img = await canvas.loadImage(`https://scalica-photos.s3.amazonaws.com/${data.Contents[i].Key}`)
-      const detections = await faceapi.detectAllFaces(img)
-      if (detections && detections.length > 0) {
-        console.log(`Detected ${detections.length} faces`)
-        
-        
-      }
-    }
-
-    con.end((err) => {
-      // The connection is terminated gracefully
-      // Ensures all previously enqueued queries are still
-      // before sending a COM_QUIT packet to the MySQL server.
-    });
-  }
-})
+    })
+  })
+}, null, true, 'America/New_York')
