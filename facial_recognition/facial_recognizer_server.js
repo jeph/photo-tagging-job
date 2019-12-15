e596dc9007bdc6a6eff9400da643edfbad0b2efc
Jeph <jephliu@jeph.io>
2019-12-15 01:15:14 -0500

    Try

--- facial_recognition/facial_recognizer_server.js
+++ facial_recognition/facial_recognizer_server.js
@@ -1,17 +1,8 @@
 const MODEL_URL = __dirname + '/weights'
 const aws = require('aws-sdk')
-let dbConfig
-if (process.env.ISPROD) {
-  dbConfig = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: 'scalica'
-  }
-} else {
-  aws.config.loadFromPath('./config.json')
-  dbConfig = require('../db-config')
-}
+
+aws.config.loadFromPath('./config.json')
+const dbConfig = require('../db-config')
 
 const s3 = new aws.S3({ apiVersion: '2006-03-01', region: 'us-east-1' })
 const faceapi = require('face-api.js')
@@ -24,48 +15,52 @@ const Bucket = 'scalica-photos'
 
 const CronJob = require('cron').CronJob
 // eslint-disable-next-line no-new
-new CronJob('*/30 * * * *', function () {
+new CronJob('* * * * *', function () {
   s3.listObjects({ Bucket }, async function (err, data) {
-    const con = mysql.createConnection(dbConfig)
-    const timeCheckSql = 'SELECT * FROM last_scan_time WHERE id = 1'
-    con.query(timeCheckSql, function (error, results) {
-      if (error) {
-        console.log(error)
-      } else {
-        const timeLastUpdated = new Date(results[0].last_scan_time).getTime() - 18000000
-        const filteredData = data.Contents.filter((file) => {
-          const fileTimeStamp = file.Key.match(/([^-]+)/g)[0]
-          return timeLastUpdated < parseInt(fileTimeStamp)
-        })
-        console.log(filteredData)
-        const timeUpdateSql = 'UPDATE last_scan_time SET last_scan_time = now(3) WHERE id = 1'
-        con.query(timeUpdateSql, async function (err) {
-          if (err) {
-            console.log(err)
-          } else {
+    if (err) {
+      console.log(err)
+    } else {
+      const con = mysql.createConnection(dbConfig)
+      const timeCheckSql = 'SELECT * FROM last_scan_time WHERE id = 1'
+      con.query(timeCheckSql, async function (error, results) {
+        if (error) {
+          console.log(error)
+        } else {
+          const timeLastUpdated = new Date(results[0].last_scan_time).getTime() - 18000000
+          const filteredData = data.Contents.filter((file) => {
+            const fileTimeStamp = file.Key.match(/([^-]+)/g)[0]
+            return timeLastUpdated < parseInt(fileTimeStamp)
+          })
+          console.log(filteredData)
+          const timeUpdateSql = 'UPDATE last_scan_time SET last_scan_time = now(3) WHERE id = 1'
+          con.query(timeUpdateSql, async function (err) {
             if (err) {
-              console.error(err, err.stack)
+              console.log(err)
             } else {
-              await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL)
-              for (let i = 0; i < filteredData.length; i++) {
-                const img = await canvas.loadImage(`https://scalica-photos.s3.amazonaws.com/${filteredData[i].Key}`)
-                const detections = await faceapi.detectAllFaces(img)
-                if (detections && detections.length > 0) {
-                  const sql = 'UPDATE micro_photo SET num_faces = ? WHERE img_id = ?'
-                  const parameters = [detections.length, filteredData[i].Key]
-                  con.query(sql, parameters, function (error) {
-                    if (error) console.log(error)
-                  })
-                  console.log(`Detected ${detections.length} faces`)
+              if (err) {
+                console.error(err, err.stack)
+              } else {
+                await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL)
+                for (let i = 0; i < filteredData.length; i++) {
+                  const img = await canvas.loadImage(`https://scalica-photos.s3.amazonaws.com/${filteredData[i].Key}`)
+                  const detections = await faceapi.detectAllFaces(img)
+                  if (detections && detections.length > 0) {
+                    const sql = 'UPDATE micro_photo SET num_faces = ? WHERE img_id = ?'
+                    const parameters = [detections.length, filteredData[i].Key]
+                    con.query(sql, parameters, function (error) {
+                      if (error) console.log(error)
+                    })
+                    console.log(`Detected ${detections.length} faces`)
+                  }
                 }
+                con.end((err) => {
+                  if (err) console.log(err)
+                })
               }
-              con.end((err) => {
-                if (err) console.log(err)
-              })
             }
-          }
-        })
-      }
-    })
+          })
+        }
+      })
+    }
   })
 }, null, true, 'America/New_York')
